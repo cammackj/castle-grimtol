aa9b483e127087a3045e262f7d1da41d03b1d6aa
Joshua Cammack <cammack110@gmail.com>
2017-09-26 09:30:15 -0600

    color change

--- Project/Game.cs
+++ Project/Game.cs
@@ -109,10 +109,12 @@ namespace CastleGrimtol.Project
 			Console.WriteLine("Press enter to start");
 			Console.ReadKey();
 			Console.Clear();
+			Console.ForegroundColor = ConsoleColor.Red;
 			Console.WriteLine("Welcome to Castle Grimtol! \n");
 			Console.WriteLine("Brave Young Warrior our forces are failing and the enemy grows stronger everyday.I fear if we don't act now our people will be driven from their homes. These dark times have left us with one final course of action. We must cut the head off of the snake by assasinating the Dark Lord of Grimtol... Our sources have identified a small tunnel that leads into the rear of the castle.\n\n" +
 			"Once you `sneak` through the tunnel you will need to find a way to disguise yourself and kill the Dark Lord. We don't know exactly how so you'll need to use your wit and cunning to think of something.\n\n" +
 			"Good Luck brave one.\n");
+			Console.ResetColor();
 		}
 
 		public void Help()
